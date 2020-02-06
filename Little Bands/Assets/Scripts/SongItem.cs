2bc36da243a967ba67427c931bf76b921834db1d
rjcarney <rcarney@emich.edu>
2020-02-05 22:29:46 -0500

    Imported Audio and Image folders
    More UI updates
    Added image and audio folders

--- Little Bands/Assets/Scripts/SongItem.cs
+++ Little Bands/Assets/Scripts/SongItem.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class SongItem : MonoBehaviour
 {
     [Header("Song Information")]
-    // public Texture albumArt;
+    public Texture albumArt;
     public string title;
     public string artist;
     public string length;
@@ -32,8 +32,17 @@ public class SongItem : MonoBehaviour
     public AudioClip recorded_drums;
     public AudioClip recorded_voice;
 
+    [Header("Badge Icons")]
+    public Texture emptyBadge;
+    public Texture guitarBadge;
+    public Texture bassBadge;
+    public Texture pianoBadge;
+    public Texture drumsBadge;
+    public Texture voiceBadge;
+
+
     [Header("List Item UI Objects")]
-    public GameObject albumArt;
+    public GameObject albumArt_image;
     public GameObject title_text;
     public GameObject artist_text;
     public GameObject length_text;
@@ -50,6 +59,13 @@ public class SongItem : MonoBehaviour
         title_text.GetComponent<UnityEngine.UI.Text>().text = title;
         artist_text.GetComponent<UnityEngine.UI.Text>().text = artist;
         length_text.GetComponent<UnityEngine.UI.Text>().text = length;
+        albumArt_image.GetComponent<UnityEngine.UI.RawImage>().texture = albumArt;
+
+        badge_guitar.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_guitar == null) ? emptyBadge : guitarBadge;
+        badge_bass.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_bass == null) ? emptyBadge : bassBadge;
+        badge_piano.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_piano == null) ? emptyBadge : pianoBadge;
+        badge_drums.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_drums == null) ? emptyBadge : drumsBadge;
+        badge_voice.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_voice == null) ? emptyBadge : voiceBadge;
 
     }
 
