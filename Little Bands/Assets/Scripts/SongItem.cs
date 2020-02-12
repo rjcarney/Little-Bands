385175f98d79a7fb341c6abd3d3e1bcf3cf32704
rjcarney <33989142+rjcarney@users.noreply.github.com>
2020-02-12 12:59:43 -0500

    Badge Change Functionality
    The instrument icons will now change when an audio clip is saved to the recorded_audio variables of the SongItem script.

--- Little Bands/Assets/Scripts/SongItem.cs
+++ Little Bands/Assets/Scripts/SongItem.cs
@@ -67,12 +67,15 @@ public class SongItem : MonoBehaviour
         badge_piano.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_piano == null) ? emptyBadge : pianoBadge;
         badge_drums.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_drums == null) ? emptyBadge : drumsBadge;
         badge_voice.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_voice == null) ? emptyBadge : voiceBadge;
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        badge_guitar.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_guitar == null) ? emptyBadge : guitarBadge;
+        badge_bass.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_bass == null) ? emptyBadge : bassBadge;
+        badge_piano.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_piano == null) ? emptyBadge : pianoBadge;
+        badge_drums.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_drums == null) ? emptyBadge : drumsBadge;
+        badge_voice.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_voice == null) ? emptyBadge : voiceBadge;
     }
 }
