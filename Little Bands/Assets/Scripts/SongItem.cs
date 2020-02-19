d1f710989d84a5f0a2c334bcafd73e888186bb04
rjcarney <33989142+rjcarney@users.noreply.github.com>
2020-02-19 11:48:46 -0500

    Sprint 1: presentation
    The recording funtion is working and is updating our UI.

--- Little Bands/Assets/Scripts/SongItem.cs
+++ Little Bands/Assets/Scripts/SongItem.cs
@@ -69,20 +69,20 @@ public class SongItem : MonoBehaviour
         length_text.GetComponent<UnityEngine.UI.Text>().text = length;
         albumArt_image.GetComponent<UnityEngine.UI.RawImage>().texture = albumArt;
 
-        badge_guitar.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_guitar == null) ? emptyBadge : guitarBadge;
-        badge_bass.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_bass == null) ? emptyBadge : bassBadge;
-        badge_piano.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_piano == null) ? emptyBadge : pianoBadge;
-        badge_drums.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_drums == null) ? emptyBadge : drumsBadge;
-        badge_voice.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_voice == null) ? emptyBadge : voiceBadge;
+        badge_guitar.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_guitar.Length <= 1) ? emptyBadge : guitarBadge;
+        badge_bass.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_bass.Length <= 1) ? emptyBadge : bassBadge;
+        badge_piano.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_piano.Length <= 1) ? emptyBadge : pianoBadge;
+        badge_drums.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_drums.Length <= 1) ? emptyBadge : drumsBadge;
+        badge_voice.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_voice.Length <= 1) ? emptyBadge : voiceBadge;
     }
 
     // Update is called once per frame
     void Update()
     {
-        badge_guitar.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_guitar == null) ? emptyBadge : guitarBadge;
-        badge_bass.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_bass == null) ? emptyBadge : bassBadge;
-        badge_piano.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_piano == null) ? emptyBadge : pianoBadge;
-        badge_drums.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_drums == null) ? emptyBadge : drumsBadge;
-        badge_voice.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_voice == null) ? emptyBadge : voiceBadge;
+        badge_guitar.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_guitar.Length <= 1) ? emptyBadge : guitarBadge;
+        badge_bass.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_bass.Length <= 1) ? emptyBadge : bassBadge;
+        badge_piano.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_piano.Length <= 1) ? emptyBadge : pianoBadge;
+        badge_drums.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_drums.Length <= 1) ? emptyBadge : drumsBadge;
+        badge_voice.GetComponent<UnityEngine.UI.RawImage>().texture = (recorded_voice.Length <= 1) ? emptyBadge : voiceBadge;
     }
 }
