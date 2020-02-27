a9ca6e221f861b9b4b38b5277b66ee9cf551e254
rjcarney <33989142+rjcarney@users.noreply.github.com>
2020-02-27 15:33:36 -0500

    Restructured GameManager Prefab
    All of the audio sources on one object was causing a few play errors on my end so I added empty child objects that now hold the different audio sources. The audio source still on the GameManager is for recording.

--- Little Bands/Assets/Scripts/GameManager.cs
+++ Little Bands/Assets/Scripts/GameManager.cs
@@ -51,6 +51,7 @@ public class GameManager : MonoBehaviour
 
     public Texture playTexture;
     public Texture stopTexture;
+    public Texture micTexture;
 
     public GameObject PlayOptions;
 
@@ -237,11 +238,11 @@ public class GameManager : MonoBehaviour
 
     // New Record page instrument buttons
     public void InstrumentButtonOnClick(string instrument) {
-        if (!recording) {
-            if (!playing_recording) {
+            if (!playing_recording && !recording) {
                 // First Click Select instrument
                 if (SelectedInstrument != instrument) {
                     SelectedInstrument = instrument;
+                sheetMusicTitle.GetComponent<UnityEngine.UI.Text>().text = SelectedSong.title + ": " + SelectedInstrument;
                 }
                 // Every Subsequent Click Toggle Audio
                 else {
@@ -391,158 +392,162 @@ public class GameManager : MonoBehaviour
                     }
                 }
             }
-        }
     }
     
     //  The Play Button will now only play the users recorded audio
      public void PlayButtonOnClick() {
-        if (playing_recording == false) {
-            // Stop all curently playing audio
-            guitarAudioSource.Stop();
-            bassAudioSource.Stop();
-            pianoAudioSource.Stop();
-            drumsAudioSource.Stop();
-            voiceAudioSource.Stop();
-            playing_layeredAudio = false;
+        if (!recording) {
+            if (playing_recording == false) {
+                // Stop all curently playing audio
+                guitarAudioSource.Stop();
+                bassAudioSource.Stop();
+                pianoAudioSource.Stop();
+                drumsAudioSource.Stop();
+                voiceAudioSource.Stop();
+                playing_layeredAudio = false;
 
-            // Set all recording clips to associated recorded wav file otherwise set to null
-            guitarAudioSource.clip = (SelectedSong.recorded_guitarClip == null) ? null : SelectedSong.recorded_guitarClip;
-            bassAudioSource.clip = (SelectedSong.recorded_bassClip == null) ? null : SelectedSong.recorded_bassClip;
-            pianoAudioSource.clip = (SelectedSong.recorded_pianoClip == null) ? null : SelectedSong.recorded_pianoClip;
-            drumsAudioSource.clip = (SelectedSong.recorded_drumsClip == null) ? null : SelectedSong.recorded_drumsClip;
-            voiceAudioSource.clip = (SelectedSong.recorded_voiceClip == null) ? null : SelectedSong.recorded_voiceClip;
+                // Set all recording clips to associated recorded wav file otherwise set to null
+                guitarAudioSource.clip = (SelectedSong.recorded_guitarClip == null) ? null : SelectedSong.recorded_guitarClip;
+                bassAudioSource.clip = (SelectedSong.recorded_bassClip == null) ? null : SelectedSong.recorded_bassClip;
+                pianoAudioSource.clip = (SelectedSong.recorded_pianoClip == null) ? null : SelectedSong.recorded_pianoClip;
+                drumsAudioSource.clip = (SelectedSong.recorded_drumsClip == null) ? null : SelectedSong.recorded_drumsClip;
+                voiceAudioSource.clip = (SelectedSong.recorded_voiceClip == null) ? null : SelectedSong.recorded_voiceClip;
 
-            if(guitarAudioSource.clip == null && bassAudioSource.clip == null &&
-                pianoAudioSource.clip == null && drumsAudioSource.clip == null &&
-                voiceAudioSource.clip == null) {
-                // All Recorded Wav Files Null Nothing to Play
-                // Maybe play avatar voice saying I need to record something first
+                if (guitarAudioSource.clip == null && bassAudioSource.clip == null &&
+                    pianoAudioSource.clip == null && drumsAudioSource.clip == null &&
+                    voiceAudioSource.clip == null) {
+                    // All Recorded Wav Files Null Nothing to Play
+                    // Maybe play avatar voice saying I need to record something first
+                } else {
+                    // There are recorded wav Files
+                    // Play what files the user has recorded
+                    if (guitarAudioSource.clip != null) {
+                        guitarAudioSource.Play();
+                        guitarAudioSource.volume = 1;
+                        guitarTrackText.GetComponent<UnityEngine.UI.Text>().text = "Student";
+                    } else {
+                        guitarTrackText.GetComponent<UnityEngine.UI.Text>().text = "None";
+                    }
+                    if (bassAudioSource.clip != null) {
+                        bassAudioSource.Play();
+                        bassAudioSource.volume = 1;
+                        bassTrackText.GetComponent<UnityEngine.UI.Text>().text = "Student";
+                    } else {
+                        bassTrackText.GetComponent<UnityEngine.UI.Text>().text = "None";
+                    }
+                    if (pianoAudioSource.clip != null) {
+                        pianoAudioSource.Play();
+                        pianoAudioSource.volume = 1;
+                        pianoTrackText.GetComponent<UnityEngine.UI.Text>().text = "Student";
+                    } else {
+                        pianoTrackText.GetComponent<UnityEngine.UI.Text>().text = "None";
+                    }
+                    if (drumsAudioSource.clip != null) {
+                        drumsAudioSource.Play();
+                        drumsAudioSource.volume = 1;
+                        drumsTrackText.GetComponent<UnityEngine.UI.Text>().text = "Student";
+                    } else {
+                        drumsTrackText.GetComponent<UnityEngine.UI.Text>().text = "None";
+                    }
+                    if (voiceAudioSource.clip != null) {
+                        voiceAudioSource.Play();
+                        voiceAudioSource.volume = 1;
+                        voiceTrackText.GetComponent<UnityEngine.UI.Text>().text = "Student";
+                    } else {
+                        voiceTrackText.GetComponent<UnityEngine.UI.Text>().text = "None";
+                    }
+
+                    playBtn.GetComponent<UnityEngine.UI.RawImage>().texture = stopTexture;
+                    playBtnTxt.GetComponent<UnityEngine.UI.Text>().text = "\n\n\nStop";
+                    playing_recording = true;
+                }
             } else {
-                // There are recorded wav Files
-                // Play what files the user has recorded
-                if (guitarAudioSource.clip != null) {
-                    guitarAudioSource.Play();
-                    guitarAudioSource.volume = 1;
-                    guitarTrackText.GetComponent<UnityEngine.UI.Text>().text = "Student";
-                } else {
-                    guitarTrackText.GetComponent<UnityEngine.UI.Text>().text = "None";
+                // Stop All Audio
+                guitarAudioSource.Stop();
+                bassAudioSource.Stop();
+                pianoAudioSource.Stop();
+                drumsAudioSource.Stop();
+                voiceAudioSource.Stop();
+
+                // Reset Audio Track to toggled Selection
+                // Reset guitar track
+                switch (SelectedSong.guitarToggleCount) {
+                    case 0: // Teacher
+                        guitarAudioSource.clip = SelectedSong.original_guitar;
+                        guitarAudioSource.volume = 1;
+                        break;
+                    case 1: // Student
+                        guitarAudioSource.clip = SelectedSong.recorded_guitarClip;
+                        guitarAudioSource.volume = 1;
+                        break;
+                    case 2: // Muted
+                        guitarAudioSource.volume = 0;
+                        break;
                 }
-                if (bassAudioSource.clip != null) {
-                    bassAudioSource.Play();
-                    bassAudioSource.volume = 1;
-                    bassTrackText.GetComponent<UnityEngine.UI.Text>().text = "Student";
-                } else {
-                    bassTrackText.GetComponent<UnityEngine.UI.Text>().text = "None";
+                // Reset bass track
+                switch (SelectedSong.bassToggleCount) {
+                    case 0:
+                        bassAudioSource.clip = SelectedSong.original_bass;
+                        bassAudioSource.volume = 1;
+                        break;
+                    case 1:
+                        bassAudioSource.clip = SelectedSong.recorded_bassClip;
+                        bassAudioSource.volume = 1;
+                        break;
+                    case 2:
+                        bassAudioSource.volume = 0;
+                        break;
                 }
-                if (pianoAudioSource.clip != null) {
-                    pianoAudioSource.Play();
-                    pianoAudioSource.volume = 1;
-                    pianoTrackText.GetComponent<UnityEngine.UI.Text>().text = "Student";
-                } else {
-                    pianoTrackText.GetComponent<UnityEngine.UI.Text>().text = "None";
+                // Reset piano track
+                switch (SelectedSong.pianoToggleCount) {
+                    case 0:
+                        pianoAudioSource.clip = SelectedSong.original_piano;
+                        pianoAudioSource.volume = 1;
+                        break;
+                    case 1:
+                        pianoAudioSource.clip = SelectedSong.recorded_pianoClip;
+                        pianoAudioSource.volume = 1;
+                        break;
+                    case 2:
+                        pianoAudioSource.volume = 0;
+                        break;
                 }
-                if (drumsAudioSource.clip != null) {
-                    drumsAudioSource.Play();
-                    drumsAudioSource.volume = 1;
-                    drumsTrackText.GetComponent<UnityEngine.UI.Text>().text = "Student";
-                } else {
-                    drumsTrackText.GetComponent<UnityEngine.UI.Text>().text = "None";
+                // Reset drums track
+                switch (SelectedSong.drumsToggleCount) {
+                    case 0:
+                        drumsAudioSource.clip = SelectedSong.original_drums;
+                        drumsAudioSource.volume = 1;
+                        break;
+                    case 1:
+                        drumsAudioSource.clip = SelectedSong.recorded_drumsClip;
+                        drumsAudioSource.volume = 1;
+                        break;
+                    case 2:
+                        drumsAudioSource.volume = 0;
+                        break;
                 }
-                if (voiceAudioSource.clip != null) {
-                    voiceAudioSource.Play();
-                    voiceAudioSource.volume = 1;
-                    voiceTrackText.GetComponent<UnityEngine.UI.Text>().text = "Student";
-                } else {
-                    voiceTrackText.GetComponent<UnityEngine.UI.Text>().text = "None";
+                // Reset voice track
+                switch (SelectedSong.voiceToggleCount) {
+                    case 0:
+                        voiceAudioSource.clip = SelectedSong.original_voice;
+                        voiceAudioSource.volume = 1;
+                        break;
+                    case 1:
+                        voiceAudioSource.clip = SelectedSong.recorded_voiceClip;
+                        voiceAudioSource.volume = 1;
+                        break;
+                    case 2:
+                        voiceAudioSource.volume = 0;
+                        break;
                 }
 
-                playBtn.GetComponent<UnityEngine.UI.RawImage>().texture = stopTexture;
-                playBtnTxt.GetComponent<UnityEngine.UI.Text>().text = "\n\n\nStop";
-                playing_recording = true;
+                playBtn.GetComponent<UnityEngine.UI.RawImage>().texture = playTexture;
+                playBtnTxt.GetComponent<UnityEngine.UI.Text>().text = "\n\n\nPlay";
+                playing_recording = false;
             }
         } else {
-            // Stop All Audio
-            guitarAudioSource.Stop();
-            bassAudioSource.Stop();
-            pianoAudioSource.Stop();
-            drumsAudioSource.Stop();
-            voiceAudioSource.Stop();
-
-            // Reset Audio Track to toggled Selection
-            // Reset guitar track
-            switch (SelectedSong.guitarToggleCount) {
-                case 0: // Teacher
-                    guitarAudioSource.clip = SelectedSong.original_guitar;
-                    guitarAudioSource.volume = 1;
-                    break;
-                case 1: // Student
-                    guitarAudioSource.clip = SelectedSong.recorded_guitarClip;
-                    guitarAudioSource.volume = 1;
-                    break;
-                case 2: // Muted
-                    guitarAudioSource.volume = 0;
-                    break;
-            }
-            // Reset bass track
-            switch (SelectedSong.bassToggleCount) {
-                case 0:
-                    bassAudioSource.clip = SelectedSong.original_bass;
-                    bassAudioSource.volume = 1;
-                    break;
-                case 1:
-                    bassAudioSource.clip = SelectedSong.recorded_bassClip;
-                    bassAudioSource.volume = 1;
-                    break;
-                case 2:
-                    bassAudioSource.volume = 0;
-                    break;
-            }
-            // Reset piano track
-            switch (SelectedSong.pianoToggleCount) {
-                case 0:
-                    pianoAudioSource.clip = SelectedSong.original_piano;
-                    pianoAudioSource.volume = 1;
-                    break;
-                case 1:
-                    pianoAudioSource.clip = SelectedSong.recorded_pianoClip;
-                    pianoAudioSource.volume = 1;
-                    break;
-                case 2:
-                    pianoAudioSource.volume = 0;
-                    break;
-            }
-            // Reset drums track
-            switch (SelectedSong.drumsToggleCount) {
-                case 0:
-                    drumsAudioSource.clip = SelectedSong.original_drums;
-                    drumsAudioSource.volume = 1;
-                    break;
-                case 1:
-                    drumsAudioSource.clip = SelectedSong.recorded_drumsClip;
-                    drumsAudioSource.volume = 1;
-                    break;
-                case 2:
-                    drumsAudioSource.volume = 0;
-                    break;
-            }
-            // Reset voice track
-            switch (SelectedSong.voiceToggleCount) {
-                case 0:
-                    voiceAudioSource.clip = SelectedSong.original_voice;
-                    voiceAudioSource.volume = 1;
-                    break;
-                case 1:
-                    voiceAudioSource.clip = SelectedSong.recorded_voiceClip;
-                    voiceAudioSource.volume = 1;
-                    break;
-                case 2:
-                    voiceAudioSource.volume = 0;
-                    break;
-            }
-
-            playBtn.GetComponent<UnityEngine.UI.RawImage>().texture = playTexture;
-            playBtnTxt.GetComponent<UnityEngine.UI.Text>().text = "\n\n\nPlay";
-            playing_recording = false;
+            // Audio is recoring this button will display the microphone texture at this time
+            // This will be happening in the update method
         }
     }
 
@@ -677,6 +682,7 @@ public class GameManager : MonoBehaviour
 
         // Toggle Track Text
         if (SelectedSong != null && !playing_recording) {
+            // playing_recording reffers to if the player is playing the audio they recorded using the play button
             switch (SelectedSong.guitarToggleCount) {
                 case 0:
                     guitarTrackText.GetComponent<UnityEngine.UI.Text>().text = "Teacher";
@@ -734,12 +740,17 @@ public class GameManager : MonoBehaviour
             }
         }
 
-        // Revert To Play Button When Audio is Finished Playing Recording
-        if (!playing_recording) {
+        // Revert To Play Button When Audio is Finished Playing Recording or Recording
+        if (!playing_recording && !recording) {
             playBtn.GetComponent<UnityEngine.UI.RawImage>().texture = playTexture;
             playBtnTxt.GetComponent<UnityEngine.UI.Text>().text = "\n\n\nPlay";
         }
 
+        // Show Mircrophone Texture on Play Button When Recording
+        if (recording) {
+            playBtn.GetComponent<UnityEngine.UI.RawImage>().texture = micTexture;
+            playBtnTxt.GetComponent<UnityEngine.UI.Text>().text = "\n\n\nRecording";
+        }
 
         if(SelectedInstrument != null) {
             switch (SelectedInstrument) {
@@ -791,7 +802,7 @@ public class GameManager : MonoBehaviour
         } else {
             // Selected Instrument is null
             // fill audio slider based on time passed
-            // check for end of song and change play buttin back
+            // check for end of song and change play button back
             // can not record without a selected instrument
         }
     }
