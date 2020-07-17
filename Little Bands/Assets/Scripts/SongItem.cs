cfcc8e6bf5cf5a4407e77411da1ad467422fd64b
rjcarney <33989142+rjcarney@users.noreply.github.com>
2020-07-17 07:31:23 -0400

    Add OnClick to newly Instantiated SongListItem

--- Little Bands/Assets/Scripts/SongItem.cs
+++ Little Bands/Assets/Scripts/SongItem.cs
@@ -80,4 +80,15 @@ public class SongItem : MonoBehaviour
         this.title = title;
         title_text.GetComponent<UnityEngine.UI.Text>().text = title;
     }
+
+    public void setAlbumArt(Texture2D tex) {
+        albumArt = tex;
+        albumArt_image.GetComponent<UnityEngine.UI.RawImage>().texture = albumArt;
+    }
+
+    public void setFullAudio(AudioClip fullAudio) {
+        original_full_audio = fullAudio;
+        length = Mathf.Floor(original_full_audio.length / 60) + ":" + (original_full_audio.length % 60);
+        length_text.GetComponent<UnityEngine.UI.Text>().text = length;
+    }
 }
--- Little Bands/Assets/Scripts/SongSpawner.cs
+++ Little Bands/Assets/Scripts/SongSpawner.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SongSpawner : MonoBehaviour
 {
@@ -28,10 +29,40 @@ public class SongSpawner : MonoBehaviour
             songInfo.setTitle(Path.GetFileName(path));
             string[] songFiles = Directory.GetFiles(path);
 
+            songInfo.setAlbumArt(LoadPNG(songFiles[0]));
+
+            AudioClipArrayCombiner audioClipArrayCombiner = this.gameObject.GetComponent<AudioClipArrayCombiner>();
+            songInfo.setFullAudio(audioClipArrayCombiner.ToAudioClip(songFiles[2]));
+
+            songInfo.instruction_bass = audioClipArrayCombiner.ToAudioClip(songFiles[4]);
+            songInfo.instruction_drums = audioClipArrayCombiner.ToAudioClip(songFiles[6]);
+            songInfo.instruction_guitar = audioClipArrayCombiner.ToAudioClip(songFiles[8]);
+            songInfo.instruction_piano = audioClipArrayCombiner.ToAudioClip(songFiles[10]);
+            songInfo.instruction_voice = audioClipArrayCombiner.ToAudioClip(songFiles[12]);
+
+            songInfo.original_bass = audioClipArrayCombiner.ToAudioClip(songFiles[14]);
+            songInfo.original_drums = audioClipArrayCombiner.ToAudioClip(songFiles[16]);
+            songInfo.original_guitar = audioClipArrayCombiner.ToAudioClip(songFiles[18]);
+            songInfo.original_piano = audioClipArrayCombiner.ToAudioClip(songFiles[20]);
+            songInfo.original_voice = audioClipArrayCombiner.ToAudioClip(songFiles[22]);
 
 
+            song.GetComponent<Button>().onClick.AddListener( delegate { this.gameObject.GetComponent<GameManager>().selectSong(song); });
         }
 
 
+    }
+
+    public static Texture2D LoadPNG(string filePath) {
+
+        Texture2D tex = null;
+        byte[] fileData;
+
+        if (File.Exists(filePath)) {
+            fileData = File.ReadAllBytes(filePath);
+            tex = new Texture2D(2, 2);
+            tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+        }
+        return tex;
     }
 }
