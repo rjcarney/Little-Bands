9e80b389d6fc948ba828a582bdd9a4627acb54a7
moconn10 <moconn13@emich.edu>
2020-08-18 18:29:31 -0400

    VideoControls
    Added a pause and play button along with a slider for videos.

---
+++ Little Bands/Assets/Scripts/Track.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class Track : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public VideoPlayer video;
+    Slider tracking;
+    bool slide = false;
+
+    void Start()
+    {
+        tracking = GetComponent<Slider>();
+    }
+
+    public void OnPointerDown(PointerEventData a)
+    {
+        slide = true;
+    }    
+
+    public void OnPointerUp(PointerEventData a)
+    {
+        float frame = (float)tracking.value * (float)video.frameCount;
+        video.frame = (long)frame;
+        slide = false;
+    }
+
+    void Update()
+    {
+        if (!slide)
+        {
+            tracking.value = (float)video.frame / (float)video.frameCount;
+        }
+    }
+}
