3b75bb7cb22dd11979082e9a4671b976be043865
moconn10 <moconn13@emich.edu>
2020-12-14 11:34:40 -0500

    AudioRead Pause
    AudioRead now adds to a recording and continues to add everytime it's called unless it is cleared.

--- Little Bands/Assets/Scripts/AudioRead.cs
+++ Little Bands/Assets/Scripts/AudioRead.cs
@@ -2,79 +2,140 @@
 using System.Collections.Generic;
 using System.IO;
 using System;
+using System.Linq;
 
-public class AudioRead : MonoBehaviour {
+public class AudioRead : MonoBehaviour
+{
     public bool startRecord = false;
     public bool isRecording = false;
+    public bool finishedRecording;
     public int frequency = 44100;
     public int seconds = 120;
     public AudioSource audioSource;
+    public AudioSource tempSource;
 
-    public List<float> tempRecording = new List<float>();
-    public float[] recordedInstrument;
+    private bool tempFull = false;
 
-    void Start() {
+    private GameManager gm;
+    private List<float> tempRecording = new List<float>();
+    private float[] recordedInstrument;
+
+    void Start()
+    {
         //Set audioSource
-        audioSource = GetComponent<AudioSource>();
-        audioSource.clip = Microphone.Start(null, true, seconds, frequency);
+        //audioSource = GetComponent<AudioSource>();
+        //audioSource.clip = Microphone.Start(null, true, seconds, frequency);
+        //tempSource = new AudioSource();
     }
 
-    void Update() {
-        if (Input.GetKeyDown("space") || startRecord) {
-            print("recording: " + isRecording);
+    void Update()
+    {
+        //Determine state of recording
+        if (startRecord)
+        {
             isRecording = !isRecording;
             startRecord = false;
-            //seconds = Mathf.CeilToInt(audioSource.clip.length);
 
-            //Create audio float array
-            if (isRecording == false) {
-                int length = Microphone.GetPosition(null);
-                Microphone.End(null);
-                float[] clipData = new float[length];
-                audioSource.clip.GetData(clipData, 0);
-
-                float[] fullClip = new float[clipData.Length + tempRecording.Count];
-                for (int i = 0; i < fullClip.Length; i++) {
-                    if (i < tempRecording.Count)
-                        fullClip[i] = tempRecording[i];
-                    else
-                        fullClip[i] = clipData[i - tempRecording.Count];
-                }
-
-                recordedInstrument = fullClip;
-                audioSource.clip = AudioClip.Create("recorded samples", fullClip.Length*2, 1, frequency, false);
-                //
-                
-                audioSource.clip.SetData(fullClip, 0);
-            }
-            //Record
-            else {
-                audioSource.Stop();
-                tempRecording.Clear();
-                Microphone.End(null);
-                audioSource.clip = Microphone.Start(null, true, seconds, frequency);
-            }
+            //Create audio clip
+            if (isRecording == false)
+                RecordAudio();
+            //Stop recording
+            else
+                PauseRecording();
         }
-        //testing
-        if (Input.GetKeyDown(KeyCode.M))
-            playAudio(recordedInstrument);
-        if (Input.GetKeyDown(KeyCode.N))
+
+        //Combine and clear tempSource
+        if (!isRecording)
         {
-            AudioWrite x = new AudioWrite();
-            x.SaveAudioClipToWav(x.convertAudio(recordedInstrument), "Test420.wav");
+            if (tempFull)
+                Combine();
+            else
+                tempSource.clip = audioSource.clip;
         }
-        if (Input.GetKeyDown(KeyCode.B))       
-            audioSource.Stop();
-        
+
+        if (Input.GetKeyDown("space"))
+            startRecord = !startRecord;
+        if (Input.GetKeyDown("z"))
+            audioSource.Play();
+        if (Input.GetKeyDown("x"))
+            tempSource.Play();
     }
 
-    //Convert float array to audio
-    void playAudio(float[] sound) {
+    private void RecordAudio()
+    {
+        int length = Microphone.GetPosition(null);
+        Microphone.End(null);
+        float[] clipData = new float[length];
+        audioSource.clip.GetData(clipData, 0);
+
+        float[] fullClip = new float[clipData.Length + tempRecording.Count];
+        for (int i = 0; i < fullClip.Length; i++)
+        {
+            if (i < tempRecording.Count)
+                fullClip[i] = tempRecording[i];
+            else
+                fullClip[i] = clipData[i - tempRecording.Count];
+        }
+
+        //Set Audio
+        recordedInstrument = fullClip;
+        audioSource.clip = AudioClip.Create("recorded samples", fullClip.Length, 1, frequency, false);
+
+        audioSource.clip.SetData(fullClip, 0);
+
+    }
+
+    public void PauseRecording()
+    {
         audioSource.Stop();
-        int length = sound.Length;
-        audioSource.clip = AudioClip.Create("recorded samples", length, 1, frequency, false);
-        audioSource.clip.SetData(sound, 0);
-        audioSource.loop = true;
-        audioSource.Play();
+        //tempRecording.Clear();
+        Microphone.End(null);
+        audioSource.clip = Microphone.Start(null, true, seconds, frequency);
+        print("Pause Recording");
+        tempFull = true;
+    }
+
+
+    private void Combine()
+    {
+        if (tempFull)
+        {
+            print("Combining");
+            //AudioClip[] clips = new AudioClip[] { audioSource.clip, tempSource.clip };
+            Combine(audioSource.clip, tempSource.clip);
+            RemoveClip(tempSource);
+            tempFull = false;
+        }
+    }
+
+    private void RemoveClip(AudioSource audio)
+    {
+        audio.clip = Microphone.Start(null, true, seconds, frequency);
+        audio.clip = null;
+    }
+
+    public void ClearAudio()
+    {
+        audioSource.clip = null;
+        tempSource.clip = null;
+    }
+
+    private void Combine(AudioClip x, AudioClip y)
+    {
+        if (x != null && y != null)
+        {
+            float[] first = new float[x.samples * x.channels];
+            float[] second = new float[y.samples * y.channels];
+            float[] final = new float[first.Length + second.Length];
+
+            x.GetData(first, 0);
+            y.GetData(second, 0);
+
+            final = second.Concat(first).ToArray();
+
+            audioSource.clip = AudioClip.Create("recorded samples", final.Length, 1, frequency, false);
+            audioSource.clip.SetData(final, 0);
+            tempSource.clip.SetData(final, 0);
+        }
     }
 }
