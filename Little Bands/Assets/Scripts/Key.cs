506e6b499d15ac1814a04c5e0cf17472089875a1
rjcarney <33989142+rjcarney@users.noreply.github.com>
2020-12-15 03:50:26 -0500

    Virtual Instruments

---
+++ Little Bands/Assets/Scripts/Key.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Key : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler {
+
+    public AudioClip note;
+    private AudioSource audioSource;
+
+    private RawImage key;
+    private Color keyColor;
+    public Color hoverColor;
+
+    // Start is called before the first frame update
+    void Start() {
+        key = GetComponent<RawImage>();
+        keyColor = key.color;
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = note;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        key.color = hoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        key.color = keyColor;
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData) {
+        audioSource.Play();
+    }
+
+}
---
+++ Little Bands/Assets/Scripts/Virtual_Instrument.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Virtual_Instrument : MonoBehaviour
+{
+    public AudioSource[] audioSources;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
